e119405134e25d0d2183feec5b3cca0cfd1f21a2
Parbej Alam <91610902+Parbej275@users.noreply.github.com>
2024-12-04 12:00:22 +0545

    Add files via upload

---
+++ main.c
@@ -0,0 +1,108 @@
+//linear enqueue 
+#include<stdio.h>
+#define SIZE 5
+int linear_queue[SIZE];
+int rear=-1, front=-1;
+void enqueue(int);
+void dequeue();
+void front_element();
+void display();
+
+int main()
+{int choice,data;
+while(1)
+{
+    
+
+    printf("\nEnter \n1 for Enqueue \n2 for dequeue \n3 for front \n4 display \n5 for exit ");
+    scanf("%d", &choice);
+    switch (choice)
+    {
+        case 1:
+        printf("\n enter data");
+        scanf("%d",&data);
+        enqueue(data);
+        break;
+        case 2:
+        dequeue();
+        break;
+        case 3:
+        front_element();
+        break;
+        case 4:
+        display();
+        case 5:
+        printf("RCB won the match bye bye");
+        return 0;
+        
+    
+        
+        
+    }}
+    return 0;
+    
+}void enqueue (int data)
+{
+    if (rear==SIZE-1)
+    {
+        printf("\n queue is already full");
+        return;
+        
+    }
+    else if(rear==-1 && front==-1)
+    {
+        rear=rear+1;
+        front=front+1;
+        
+    }
+    else
+    {
+        rear=rear+1;
+        
+    }
+    linear_queue[rear]=data;
+    printf("\n %d is enqueued ",data);
+    
+}
+void dequeue()
+{
+    if (rear==-1 && front==-1)
+    {
+        printf("\n queue is already empty");
+    } else if
+    (rear==front)
+    {
+        printf ("\n %d is deueued succcesfully ",linear_queue[front]);
+        rear=-1;
+        front=-1;
+        
+    }
+    else 
+    {
+        printf("\n %d is deueued succesfully",linear_queue[front]);
+        front=front+1;
+    }
+}
+void front_element()
+{
+    if (rear==-1 && front==-1)
+    {
+        printf("\n %d is front successfully",linear_queue[front]);
+    }
+}
+void display()
+{
+    int i;
+    if (front==-1 && front==-1)
+    {
+        printf("\n queue is empty");
+        
+    }
+    else 
+    {
+        for (i=front; i<=rear; i++)
+        {
+            printf("\n %d", linear_queue[i]);
+        }
+    }
+}
